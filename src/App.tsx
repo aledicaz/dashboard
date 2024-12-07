9b4fdd3f2d2f81ec2d71ddd650367723376fa41c
Alex <licazago@fiec.espol.edu.ec>
2024-12-07 16:19:22 -0500

    STOP 2 Guia 12

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
 //import viteLogo from '/vite.svg'
 import './App.css'
 import Grid from '@mui/material/Grid2' 
+import IndicatorWeather from './components/IndicatorWeather';
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -11,10 +12,21 @@ function App() {
     <Grid container spacing={5}>
 
         {/* Indicadores */}
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 1</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 2</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 3</Grid>
-        <Grid size={{ xs: 12, xl: 3 }}>Elemento: Indicador 4</Grid>
+        <Grid size={{ xs: 12, xl: 3 }}>
+          <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} /> 
+        </Grid>
+
+        <Grid size={{ xs: 12, xl: 3 }}>
+          <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
+        </Grid>
+
+        <Grid size={{ xs: 12, xl: 3 }}>
+          <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
+        </Grid>
+
+        <Grid size={{ xs: 12, xl: 3 }}>
+          <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
+        </Grid>
 
         {/* Tabla */}
         <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
---
+++ src/components/IndicatorWeather.tsx
@@ -0,0 +1,15 @@
+interface Indicator {
+    title?: String;
+    subtitle?: String;
+    value?: String;
+}
+
+export default function IndicatorWeather(config: Indicator) {
+    return (
+        <>
+            {config.title}<br />
+            {config.value}<br />
+            {config.subtitle}
+        </>
+    )
+}
