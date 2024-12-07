15fdf34322d0fb3ccba6d7c5018e2834938bd18b
Alex <licazago@fiec.espol.edu.ec>
2024-12-07 16:33:33 -0500

    STOP 5 Guia 12

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Grid from '@mui/material/Grid2'
 import IndicatorWeather from './components/IndicatorWeather';
 import TableWeather from './components/TableWeather';
 import ControlWeather from './components/ControlWeather';
+import LineChartWeather from './components/LineChartWeather';
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -44,8 +45,11 @@ function App() {
         </Grid>
       </Grid>
 
+
       {/* Gráfico */}
-      <Grid size={{ xs: 12, xl: 4 }}>Elemento: Gráfico 1</Grid>
+      <Grid size={{ xs: 12, xl: 4 }}>
+        <LineChartWeather/>
+      </Grid>
 
     </Grid>
   )
